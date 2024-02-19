8aa2a560b0b4ff517ad11a7e9896488d41845a74
TecNM <TecNM@DESKTOP-BABKH5A>
2024-02-19 15:54:02 -0600

    Agregar archivos de proyecto.

---
+++ GravitationalForce/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FormulaFisica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            double mass1 = 10.0;
+            double mass2 = 5.0; 
+            double distance = 3.0;
+           
+            double G = 6.67430e-11;
+
+            //Gravitational Force Formula : F = G * (m1 * m2) / r^2
+            double fuerza = G * (mass1 * mass2) / Math.Pow(distance, 2);
+
+         
+            Console.WriteLine($"La fuerza gravitacional entre los objetos es {fuerza} newtons.");
+        }
+    }
+}
